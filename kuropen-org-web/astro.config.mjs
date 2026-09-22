da4f5f49e1ca27d5a0e97bc13d79144bb210ef46
Hirochika Yuda <hyuda@kuropen.me>
2026-09-23 08:44:59 +0900

    Ready for Vercel

--- kuropen-org-web/astro.config.mjs
+++ kuropen-org-web/astro.config.mjs
@@ -2,17 +2,14 @@
 import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   fonts: [
     {
       provider: fontProviders.fontsource(),
